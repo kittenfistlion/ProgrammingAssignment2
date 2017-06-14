4d07991785479a3906204a3aab33ed934f74ab75
Kitten fist lion <climba00039@gmail.com>
2017-06-14 18:44:42 +0800

    finial version of functions: inverse of matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The below two functions are designed to be used together so as to calculate the inverse of matrix 
 
-## Write a short comment describing this function
+## It is an environment in which the matrix and inverse of matrix's values are stored and provide a list of getter and setter function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  IM <- NULL
+  ##set the matrix and clear the dated matrix at the same time  
+  set <- function(new_matrix){
+    x <<- new_matrix
+    IM <<- NULL
+  }
+  ## giving out the value of matrix 
+  give <- function() x
+  
+  ## A way to store the inverse of matrix in this function environment 
+  Input <- function(z) IM <<- Z 
+  
+  ## checkout the inverse of matrix
+  Output <- function() IM 
+  
+  list(set=set, give=give,Input=Input, Output=Output)
 }
 
 
-## Write a short comment describing this function
+## it is used exclusively by the function makeCacheMatrix to calculate the inverse of matrix 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  ans <- x$Output
+  if(!is.null(ans)) {
+    message("getting catched data")
+    return(ans)
+  }
+  matrix <- x$give
+  ans <- solve(matrix)
+  x$Input(ans)
+  ans
 }
+
+
+
+
+
+
+
